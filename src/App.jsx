17cd12466965e33953592e9e8dc31dfd44d0ee44
Zee Jay <zj.bulawan@gmail.com>
2025-02-11 16:08:48 +0800

    Update: Adding properties to component instances

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ export default function App() {
       <Joke
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
+        isPun={true}
       />
       <Joke
         setup="How did the hacker escape the police?"
         punchline="He just ransomware!"
+        isPun={true}
       />
       <Joke
         setup="Why don't pirates travel on mountain roads?"
         punchline="Scurvy."
+        isPun={true}
       />
       <Joke
         setup="Why do bees stay in the hive in the winter?"
         punchline="Swarm."
+        isPun={true}
       />
       <Joke
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
+        isPun={false}
       />
     </>
   )
