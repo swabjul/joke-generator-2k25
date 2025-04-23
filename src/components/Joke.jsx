24d6f182ab528aa8a3f1df4595a0f62587eb8216
Zee Jay <zj.bulawan@gmail.com>
2025-04-23 12:03:56 +0800

    Adding conditional rendering using &&

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,12 +1,20 @@
-
+import { useState } from "react"
 
 export default function Joke(props) {
-
   const {setup, punchline} = props
+
+  const [isShown, setIsShown] = useState(false)
+
+  function toggleShown() {
+    setIsShown(prevShow => !prevShow)
+  }
+
   return (
     <>
       <h3>{setup}</h3>
-      <p>{punchline}</p>
+      {isShown && <p>{punchline}</p>}
+      <button onClick={toggleShown}>Show punchline</button>
+      <hr/>
     </>
   )
 }
